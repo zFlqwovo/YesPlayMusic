2c233be4d6af734b2b52700411f0da1fa8ec6809
zFlqwovo <118718052+zFlqwovo@users.noreply.github.com>
2026-06-17 23:13:40 +0800

    Sync

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM node:16.13.1-alpine AS build
+ENV VUE_APP_NETEASE_API_URL=/api
+WORKDIR /app
+RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories &&\
+	apk add --no-cache python3 make g++ git
+COPY package.json yarn.lock ./
+RUN yarn config set electron_mirror https://npmmirror.com/mirrors/electron/ && \
+    yarn config set registry https://registry.npmmirror.com && \
+    sed -i 's/registry.yarnpkg.com/registry.npmmirror.com/g' yarn.lock && \
+    sed -i 's/registry.npmjs.org/registry.npmmirror.com/g' yarn.lock && \
+    yarn install
+COPY . .
+RUN yarn build
+
+FROM nginx:1.20.2-alpine AS app
+
+COPY --from=build /app/package.json /usr/local/lib/
+
+RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories \
+  && apk add --no-cache libuv nodejs npm \
+  && npm config set registry https://registry.npmmirror.com \
+  && npm i -g $(awk -F \" '{if($2=="@neteaseapireborn/api@latest") print $2"@"$4}' /usr/local/lib/package.json) \
+  && rm -f /usr/local/lib/package.json
+
+COPY --from=build /app/docker/nginx.conf.example /etc/nginx/conf.d/default.conf
+COPY --from=build /app/dist /usr/share/nginx/html
+
+CMD ["sh", "-c", "nginx && exec npx @neteaseapireborn/api@latest"]
---
+++ src/utils/lyrics.js
@@ -0,0 +1,114 @@
+export function lyricParser(lrc) {
+  return {
+    lyric: parseLyric(lrc?.lrc?.lyric || ''),
+    tlyric: parseLyric(lrc?.tlyric?.lyric || ''),
+    romalyric: parseLyric(lrc?.romalrc?.lyric || ''),
+    lyricuser: lrc.lyricUser,
+    transuser: lrc.transUser,
+  };
+}
+
+// regexr.com/6e52n
+const extractLrcRegex =
+  /^(?<lyricTimestamps>(?:\[.+?\])+)(?!\[)(?<content>.+)$/gm;
+const extractTimestampRegex =
+  /\[(?<min>\d+):(?<sec>\d+)(?:\.|:)*(?<ms>\d+)*\]/g;
+
+/**
+ * @typedef {{time: number, rawTime: string, content: string}} ParsedLyric
+ */
+
+/**
+ * Parse the lyric string.
+ *
+ * @param {string} lrc The `lrc` input.
+ * @returns {ParsedLyric[]} The parsed lyric.
+ * @example parseLyric("[00:00.00] Hello, World!\n[00:00.10] Test\n");
+ */
+export function parseLyric(lrc) {
+  /**
+   * A sorted list of parsed lyric and its timestamp.
+   *
+   * @type {ParsedLyric[]}
+   * @see binarySearch
+   */
+  const parsedLyrics = [];
+
+  /**
+   * Find the appropriate index to push our parsed lyric.
+   * @param {ParsedLyric} lyric
+   */
+  const binarySearch = lyric => {
+    let time = lyric.time;
+
+    let low = 0;
+    let high = parsedLyrics.length - 1;
+
+    while (low <= high) {
+      const mid = Math.floor((low + high) / 2);
+      const midTime = parsedLyrics[mid].time;
+      if (midTime === time) {
+        return mid;
+      } else if (midTime < time) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+
+    return low;
+  };
+
+  for (const line of lrc.trim().matchAll(extractLrcRegex)) {
+    const { lyricTimestamps, content } = line.groups;
+
+    for (const timestamp of lyricTimestamps.matchAll(extractTimestampRegex)) {
+      const { min, sec, ms } = timestamp.groups;
+      const validMs = ms?.slice(0, 2) ?? '00';
+      const rawTime = `[${min}:${sec}.${validMs}]`;
+      const time = Number(min) * 60 + Number(sec) + Number(validMs) * 0.01;
+
+      /** @type {ParsedLyric} */
+      const parsedLyric = { rawTime, time, content: trimContent(content) };
+      parsedLyrics.splice(binarySearch(parsedLyric), 0, parsedLyric);
+    }
+  }
+
+  return parsedLyrics;
+}
+
+/**
+ * @param {string} content
+ * @returns {string}
+ */
+function trimContent(content) {
+  let t = content.trim();
+  return t.length < 1 ? content : t;
+}
+
+/**
+ * @param {string} lyric
+ */
+export async function copyLyric(lyric) {
+  const textToCopy = lyric;
+  if (navigator.clipboard && navigator.clipboard.writeText) {
+    try {
+      await navigator.clipboard.writeText(textToCopy);
+    } catch (err) {
+      alert('复制失败，请手动复制！');
+    }
+  } else {
+    const tempInput = document.createElement('textarea');
+    tempInput.value = textToCopy;
+    tempInput.style.position = 'absolute';
+    tempInput.style.left = '-9999px';
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    try {
+      document.execCommand('copy');
+    } catch (err) {
+      alert('复制失败，请手动复制！');
+    }
+    document.body.removeChild(tempInput);
+  }
+}
